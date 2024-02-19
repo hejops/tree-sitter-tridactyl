f177b6938206f121d1b5138f0dabf5d9ba4ec619
Joseph <hejops1@gmail.com>
2024-02-19 23:40:22 +0000

    add excmd, bind_mode

--- grammar.js
+++ grammar.js
@@ -1,5 +1,6 @@
 // experimental tridactyl grammar for tree-sitter
 
+// https://github.com/tridactyl/tridactyl
 // https://github.com/tridactyl/vim-tridactyl
 
 // references:
@@ -7,6 +8,8 @@
 // https://gist.github.com/Aerijo/df27228d70c633e088b0591b8857eeef#walkthrough
 // https://github.com/tree-sitter/tree-sitter-bash/blob/975bc70ad95dbbf2733872bc2e0a059c055db983/grammar.js#L121
 // https://github.com/tree-sitter/tree-sitter/blob/master/docs/section-3-creating-parsers.md#the-grammar-dsl
+// https://github.com/tridactyl/vim-tridactyl/blob/master/syntax/tridactyl.vim#L19
+// https://siraben.dev/2022/03/01/tree-sitter.html
 // https://tree-sitter.github.io/tree-sitter/creating-parsers#getting-started
 
 module.exports = grammar({
@@ -16,12 +19,62 @@ module.exports = grammar({
   // tree-sitter test (verify that parsed tokens yield the correct ast)
 
   rules: {
-    program: ($) => optional($._statements),
-    _statements: ($) => seq(repeat(seq($.binding)), $.binding),
+    program: ($) => repeat($.statement), // zero or more statements
 
-    binding: ($) => seq("bind", $.key, $.action),
+    comment: (_) => token(prec(-10, /".*/)),
+
+    statement: ($) =>
+      choice(
+        //
+
+        $.binding,
+        // $.alias,
+        // $.command,
+        // $.setting,
+      ),
+
+    excmd: ($) =>
+      seq(
+        //
+        choice(
+          // TODO: get from https://github.com/tridactyl/tridactyl/blob/master/src/excmds.ts
+
+          "composite",
+          "scrollpage",
+          "urlparent",
+        ),
+        optional($.excmd_args),
+      ),
+    excmd_args: (_) => token(prec(-10, /.*/)), // junk for now
+
+    binding: ($) =>
+      seq(
+        "bind", //
+        optional($.bind_mode),
+        $.key,
+        $.excmd,
+      ),
+
     key: ($) => /[A-Za-z]/,
-    action: ($) => "urlparent",
+    bind_mode: ($) =>
+      seq(
+        "--mode=", //
+        token.immediate(
+          // TODO: there are a few places where modes are declared: mode2maps,
+          // ModeName, modesubconfigs. they are all slightly different from one
+          // another, so i need to check which set is considered definitive.
+          choice(
+            "normal",
+            "ignore",
+            "insert",
+            "input",
+            "ex",
+            "hint",
+            "visual",
+            "browser",
+          ),
+        ),
+      ),
 
     //
   },
